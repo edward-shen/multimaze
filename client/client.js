50ad10584333ad59966cf45beaf0819b13744343
edward-shen <eddie22099@gmail.com>
2016-06-18 13:45:39 -0400

    Code commenting

--- client/client.js
+++ client/client.js
@@ -1,8 +1,9 @@
 
-var xSize = 31, ySize = 21;
+var xSize = 31, ySize = 21; // TODO: Define by server
 
+// Because we double the size, what happens is that a natural border is generated on the right and bottom edges, since we generate the playing field by getting two points and essentially drawing between them, and there cannot be a point outside the field. This means that if we want to create a border on all edges, we need to make the array one larger.
 var playingField = createArray(ySize * 2 + 1, xSize * 2 + 1);
-var randomSeed = "setset"; // Debug variable when defined. Should be defined by server later
+var randomSeed = "setset"; // Debug variable when defined. TODO: Define by server
 
 /* Yay helper functions (from StackOverflow).
  * Creates an n-dimensional array, with n equaling the number of parameters provided
@@ -20,44 +21,51 @@ function createArray(length) {
     return arr;
 }
 
+// This uses a modified Kruskal's algorithm to generate a random maze, with vertices in a unsigned rectangular system.
 function generateMaze() {
 
     var i; // Loop variable
-    //Math.seedrandom(randomSeed);
+    Math.seedrandom(randomSeed);
     var numNodes = ySize * xSize;
 
     var representativeArray = createArray(numNodes),
-        edgeArray = createArray(2 * numNodes);
+        edgeArray = createArray(2 * numNodes); // 2 * numNodes because at most each node will have 2 connections, one going right and one going down
 
     // Initializes our representative array; their IDs are just arbitrary values
     for (i = 0; i < representativeArray.length; i++) {
         representativeArray[i] = i;
     }
 
-    console.log(edgeArray.length);
-
     // Initalizes our edgeArray
     for (i = 0; i < edgeArray.length / 2; i++){
+        // There's some confunkery occuring here. We define each edge as an object with a random weight, a starting position, and ending position.
+        // However, to obtain those positions, which are defined in a 2D space, in a 1D array, we can find our y position by floor dividing our iterator variable, and our x position would be the remainder of said floor division, represented by the modulus opertation.
+        // After that, then it merely becomes checks to see if the node to the right or to the bottom, respectively, is valid, and if so, create an edge. Otherwise, we move on to the next position
 
+        // If possible, add an edge connecting the current vertex to the one right next to it.
         if (i % xSize + 1 !== xSize) {
             edgeArray[i] = {weight: Math.random(), startPos: {y: Math.floor(i / xSize), x: i % xSize}, endPos: {y: Math.floor(i / xSize), x: (i % xSize) + 1}};
         }
 
+        // If possible, add an edge connecting the current vertex to the one below it.
         if (Math.floor(i / xSize) + 1 !== ySize) {
             edgeArray[numNodes + i] = {weight: Math.random(), startPos: {y: Math.floor(i / xSize), x: i % xSize}, endPos: {y: Math.floor(i / xSize) + 1, x: (i % xSize)}};
         }
     }
 
 
-    // Sorts edges by their weight property
+    // Sorts edges by their weight property, least to greatest
     edgeArray.sort(function(a, b) {
         return a.weight - b.weight;
     });
 
+    // Generates and draws the minimal standing tree, taking advantage of the fact that the list is sorted by least to greatest weight
     while (typeof edgeArray[0] !== 'undefined') {
+        // First, we get the data of the least-weight value...
         var startPos = edgeArray[0].startPos.y * xSize + edgeArray[0].startPos.x,
             endPos = edgeArray[0].endPos.y * xSize + edgeArray[0].endPos.x;
 
+            // ... set all IDs with the same value to equal the value of the starting node, and thus creating a representative ID subgraph ...
             var representative = representativeArray[startPos];
             var toBeReplaced = representativeArray[endPos];
             if (representative !== toBeReplaced) {
@@ -66,39 +74,45 @@ function generateMaze() {
                         representativeArray[i] = representative;
                     }
                 }
-                drawField();
+
+                drawField(); // ... draw the field...
             }
 
-        edgeArray.shift();
+        edgeArray.shift(); // ... and finally remove that node from our list, allowing us to repeat this process
     }
 
+    //
     function drawField() {
         console.log(edgeArray[0]);
         var startPos = edgeArray[0].startPos,
             endPos = edgeArray[0].endPos;
 
+        // variable below have their values incremented to account for the border we want to draw on the top and left side.
         var startPosX = startPos.x * 2 + 1,
             startPosY = startPos.y * 2 + 1,
             endPosX = endPos.x * 2 + 1;
             endPosY = endPos.y * 2 + 1;
 
-            playingField[startPosY][startPosX] = 1;
-            playingField[(startPosY + endPosY) / 2][(startPosX + endPosX) / 2] = 1;
-            playingField[endPosY][endPosX] = 1;
+            playingField[startPosY][startPosX] = 1; // Draws the box at the starting vertex
+            playingField[(startPosY + endPosY) / 2][(startPosX + endPosX) / 2] = 1; // Draws the box at the middle position, the "edge"
+            playingField[endPosY][endPosX] = 1; // Draws the box at the ending vertex
         }
 };
 
-generateMaze();
-
-// Just writes what our playing field is
-document.write("<div style=\"line-height:0\">");
-for (i=0; i < playingField.length; i++) {
-   for (j = 0; j < playingField[i].length; j++) {
-       if (typeof playingField[i][j] !== 'undefined')
-            document.write("<img src=\"black.png\"/>");
-        else
-        document.write("<img src=\"grey.png\"/>");
+// Writes the maze to the DOM, allowing us to visualize the output. Paths are black while the borders are grey
+function displayMaze() {
+    document.write("<div style=\"line-height:0\">");
+    for (i=0; i < playingField.length; i++) {
+       for (j = 0; j < playingField[i].length; j++) {
+           if (typeof playingField[i][j] !== 'undefined')
+                document.write("<img src=\"black.png\"/>");
+            else
+            document.write("<img src=\"grey.png\"/>");
+        }
+       document.write("<br/>");
     }
-   document.write("<br/>");
+    document.write("</div>");
 }
-document.write("</div>");
+
+generateMaze();
+displayMaze();
